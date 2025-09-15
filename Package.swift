a3b5680a627fde02c710470b5216116bb7184a8b
Zhassulan Aimukhambetov <57430894+ZhassulanAimukhambetov@users.noreply.github.com>
2025-09-15 16:48:01 +0500

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["MyLibraryB"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.9.1")
+        .package(url: "https://github.com/ZhassulanAimukhambetov/Alamofire.git", branch: "master")
     ],
     targets: [
         .target(
